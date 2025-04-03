4ac2383c1f6ae8e175c886dd5ca1341b41e38611
cesaryuan <cesaryuan@qq.com>
2025-04-03 10:15:58 +0800

    Update EntropyCropper to enable frequent reading from canvas context

--- src/entropy-crop.ts
+++ src/entropy-crop.ts
@@ -9,7 +9,7 @@ export class EntropyCropper {
 
     constructor() {
         this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext('2d', { willReadFrequently: true });
     }
 
     /**
