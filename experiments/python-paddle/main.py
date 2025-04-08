589f741686bd32547900ef2427a2c9ea670a0758
cesaryuan <cesaryuan@qq.com>
2025-04-08 09:34:57 +0800

    Remove python experiment

--- experiments/python-paddle/main.py
+++ experiments/python-paddle/main.py
@@ -4,5 +4,3 @@ model = create_model(model_name="PP-LCNet_x1_0_doc_ori")
 output = model.predict(r"experiments\onnx-demo\180-2.png", batch_size=1)
 for res in output:
     res.print(json_format=False)
-    res.save_to_img("./output/demo.png")
-    res.save_to_json("./output/res.json")
